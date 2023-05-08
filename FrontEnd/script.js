b8156398bca7eb0b52890094081b2ddf6b75f663
Emma B <emm2111@hotmail.fr>
2023-05-08 21:46:13 +0200

    Affichage des Projets

---
+++ FrontEnd/script.js
@@ -0,0 +1,39 @@
+
+async function loadWorks() {
+    const response = await fetch("http://localhost:5678/api/works")
+    const works = await response.json()
+    console.log(works)
+    return works
+}
+
+
+function displayWorks(works) {
+    for (let i = 0; i < works.length; i++) {
+        const figure = works[i];
+        // Récupération de l'élément du DOM qui accueillera les projets
+        const sectionGallery = document.querySelector(".gallery");
+        // Création d’une balise dédiée à un projet
+        const workElement = document.createElement("figure");
+        // Création des balises 
+        const imageElement = document.createElement("img");
+        imageElement.src = figure.imageUrl;
+        const nomElement = document.createElement("figcaption");
+        nomElement.innerText = figure.title;
+        // On rattache la balise figure a la section Projets
+        sectionGallery.appendChild(workElement);
+        workElement.appendChild(imageElement);
+        workElement.appendChild(nomElement);
+    }
+}
+
+
+async function init() {
+    // on veut récupérer la liste des works
+    const works = await loadWorks()
+    // on veut afficher la liste des works dans la page
+    displayWorks(works)
+}
+
+init()
+
+
